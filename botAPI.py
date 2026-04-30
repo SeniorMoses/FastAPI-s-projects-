6c2273a851d6c0b9c11b6347fb0f32ba865ef122
SeniorMoses <mosessenior11@gmail.com>
2026-04-30 03:18:31 +0000

    Update botAPI.py

--- botAPI.py
+++ botAPI.py
@@ -1,6 +1,22 @@
-from fastapi import FastAPI
+
+from fastapi import FastAPI 
+from fastapi.middleware.cors import CORSMiddleware
 import json
 from thefuzz import fuzz
+
+app = FastAPI()
+
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"], 
+    allow_credentials=True,
+    allow_methods=["*"],  
+    allow_headers=["["*"]"], 
+)
+
+
+
 
 app = FastAPI()
 
