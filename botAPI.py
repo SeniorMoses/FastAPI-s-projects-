445977ef0436ab0b60c8c410d443dc90bb9de5f3
SeniorMoses <mosessenior11@gmail.com>
2026-05-14 00:33:38 +0000

    Update botAPI.py

--- botAPI.py
+++ botAPI.py
@@ -10,7 +10,7 @@ app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"], 
-    allow_credentials=True,
+    allow_credentials=False,
     allow_methods=["*"],  
     allow_headers=["*"], 
 )
@@ -18,7 +18,7 @@ app.add_middleware(
 
 
 
-app = FastAPI()
+
 
 response = []
 
